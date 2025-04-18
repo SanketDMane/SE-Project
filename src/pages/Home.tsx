cbf371b102e69cb396ca079eac4d396bc2581a8f
SanketDMane <ironmanmarks456@gmail.com>
2025-04-18 21:16:33 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -94,7 +94,7 @@ export const Home = () => {
                 to="/products"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Hit the Home Page
+                Hit the Product Page
               </Link>
               <Link
                 to="/admin"
